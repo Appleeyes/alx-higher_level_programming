eab8d556359fa78271776d5d1e4ff0a7ee21d369
Appleeyes <appleeyespackage@gmail.com>
2023-03-14 17:44:12 -0700

    Task 7 updated

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0,) * (2 - len(tuple_a))
+    tuple_b += (0,) * (2 - len(tuple_b))
     sum_a = tuple_a[0] + tuple_b[0]
     sum_b = tuple_a[1] + tuple_b[1]
     return (sum_a, sum_b)
