733d925a22d42d0b068d9909498eb51b6be6df84
Appleeyes <appleeyespackage@gmail.com>
2023-03-16 19:34:42 -0700

    Task 8 to 12 added

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+
+    result = 0
+    prev = None
+
+    for letter in roman_string:
+        if letter not in roman_dict:
+            return 0
+        value = roman_dict[letter]
+
+        if prev is not Nine and prev < value:
+            result -= prev
+            value -= prev
+        result += value
+        prev = value
+    return result
