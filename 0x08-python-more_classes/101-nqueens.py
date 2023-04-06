bcc18d1c7623821d00c9bad1363ebf53ad179d87
Apple <appleeyespackage@gmail.com>
2023-04-06 02:48:11 +0100

    Task 10 added

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+import sys
+
+def nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    def solve(queens, xy_diff, xy_sum):
+        x = len(queens)
+        if x == n:
+            solutions.append(queens)
+            return
+        for y in range(n):
+            if y not in queens and x-y not in xy_diff and x+y not in xy_sum:
+                solve(queens+[y], xy_diff+[x-y], xy_sum+[x+y])
+
+    solutions = []
+    solve([], [], [])
+    for sol in solutions:
+        print(sol)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    nqueens(n)
