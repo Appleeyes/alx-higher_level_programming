b287300f2d091ff8f932877baf500cdda8cc9f0d
Appleeyes <appleeyespackage@gmail.com>
2023-04-05 18:15:12 -0700

    Task 7 pycodestyle checked

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -58,7 +58,8 @@ class Rectangle:
         if self.width == 0 or self._height == 0:
             return ''
         else:
-            return "\n".join([str(self.print_symbol) * self.width] * self.height)
+            return "\n".join([str(self.print_symbol)
+                        * self.width] * self.height)
 
     def __repr__(self):
         """returns a string representation of the rectangle"""
